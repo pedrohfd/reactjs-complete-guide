4fdd617c2ed8d100ea22c41a357699887c3a9c90
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-06-22 15:57:23 -0300

    feat: finish aplication and 5th module is complete

--- app01/src/components/NewExpense/ExpenseForm/index.js
+++ app01/src/components/NewExpense/ExpenseForm/index.js
@@ -6,6 +6,7 @@ const ExpenseForm = props => {
   const [enteredTitle, setEnteredTitle] = useState('')
   const [enteredAmount, setEnteredAmount] = useState('')
   const [enteredDate, setEnteredDate] = useState('')
+  const [switchForm, setSwitchForm] = useState(false)
 
   const handleTitleChange = event => {
     setEnteredTitle(event.target.value)
@@ -24,52 +25,67 @@ const ExpenseForm = props => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     }
 
     props.onSaveExpenseData(expenseData)
+    setSwitchForm(false)
     setEnteredTitle('')
     setEnteredAmount('')
     setEnteredDate('')
   }
 
+  const handleSwitchForm = () => {
+    setSwitchForm(!switchForm)
+    console.log(switchForm)
+  }
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div className='new-expense__controls'>
-        <div className='new-expense__control'>
-          <label>Title</label>
-          <input
-            type='text'
-            value={enteredTitle}
-            onChange={handleTitleChange}
-          />
+    <>
+      {switchForm ? (
+        <form onSubmit={handleSubmit}>
+          <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+              <label>Title</label>
+              <input
+                type='text'
+                value={enteredTitle}
+                onChange={handleTitleChange}
+              />
+            </div>
+            <div className='new-expense__control'>
+              <label>Amount</label>
+              <input
+                type='number'
+                min='0.01'
+                step='0.01'
+                value={enteredAmount}
+                onChange={handleAmountChange}
+              />
+            </div>
+            <div className='new-expense__control'>
+              <label>Date</label>
+              <input
+                type='date'
+                min='2019-01-01'
+                max='2022-12-31'
+                value={enteredDate}
+                onChange={handleDateChange}
+              />
+            </div>
+          </div>
+          <div className='new-expense__actions'>
+            <button onClick={handleSwitchForm}>Cancel</button>
+            <button type='submit'>Add Expense</button>
+          </div>
+        </form>
+      ) : (
+        <div>
+          <button onClick={handleSwitchForm}>Add New Expense</button>
         </div>
-        <div className='new-expense__control'>
-          <label>Amount</label>
-          <input
-            type='number'
-            min='0.01'
-            step='0.01'
-            value={enteredAmount}
-            onChange={handleAmountChange}
-          />
-        </div>
-        <div className='new-expense__control'>
-          <label>Date</label>
-          <input
-            type='date'
-            min='2019-01-01'
-            max='2022-12-31'
-            value={enteredDate}
-            onChange={handleDateChange}
-          />
-        </div>
-      </div>
-      <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
-      </div>
-    </form>
+      )}
+    </>
   )
 }
 
