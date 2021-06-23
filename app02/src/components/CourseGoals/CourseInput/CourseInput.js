192e018749c0f31bdc127f8b81d20e8fb5fe60ae
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-06-23 11:32:03 -0300

    feat: 6th module is done.

--- app02/src/components/CourseGoals/CourseInput/CourseInput.js
+++ app02/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from '../../UI/Button/Button'
+// import styles from './CourseInput.module.css'
+
+import { FormControl } from './styles'
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState('')
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true)
+    }
+    setEnteredValue(event.target.value)
+  }
 
   const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    event.preventDefault()
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false)
+      return
+    }
+    props.onAddGoal(enteredValue)
+  }
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <FormControl
+        invalid={!isValid}
+        // className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
+        <input type='text' onChange={goalInputChangeHandler} />
+      </FormControl>
+      <Button type='submit'>Add Goal</Button>
     </form>
-  );
-};
+  )
+}
 
-export default CourseInput;
+export default CourseInput
