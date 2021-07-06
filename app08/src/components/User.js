2db47895863ff45fbfd98bdbd57d82da29f20ecf
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-07-06 16:30:14 -0300

    feat: working with class components

--- app08/src/components/User.js
+++ app08/src/components/User.js
@@ -1,7 +1,13 @@
-import classes from './User.module.css';
+import { Component } from 'react'
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from './User.module.css'
 
-export default User;
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>
+  }
+}
+
+export default User
+
+// 3217-9257
