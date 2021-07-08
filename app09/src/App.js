5eb7ff1b3b8d0385b984358d90b12f809e4a1a70
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-07-08 10:16:23 -0300

    feat: 14th module is done

--- app09/src/App.js
+++ app09/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react'
 
-import AddMovie from './components/AddMovie'
 import MoviesList from './components/MoviesList'
+import AddMovie from './components/AddMovie'
 import './App.css'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
-  const fetchMovieHandler = useCallback(async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true)
     setError(null)
 
@@ -23,16 +23,18 @@ function App() {
 
       const data = await response.json()
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        }
-      })
-      setMovies(transformedMovies)
-      setIsLoading(false)
+      const loadedMovies = []
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      setMovies(loadedMovies)
     } catch (error) {
       setError(error.message)
     }
@@ -40,8 +42,23 @@ function App() {
   }, [])
 
   useEffect(() => {
-    fetchMovieHandler()
-  }, [fetchMovieHandler])
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler])
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      'https://react-http-64d27-default-rtdb.firebaseio.com/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    const data = await response.json()
+    console.log(data)
+  }
 
   let content = <p>Found no movies.</p>
 
@@ -60,10 +77,10 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <AddMovie />
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
     </React.Fragment>
